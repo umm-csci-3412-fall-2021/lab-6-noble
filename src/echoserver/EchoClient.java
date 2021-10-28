a235a12e728cdb7936263021c8fab26bfb2feef0
Kyle Day <day00090@morris.umn.edu>
2021-10-28 10:39:16 -0500

    Update EchoClient.java by Joining Starter and Reference Code
    In this commit, I synthesize the last lab's code I brought in for reference with the starter code for this lab. To do this, I set the server to being "localhost" at all times and split the main method into another helper method: start(). This will make way for the creation of threads.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -6,14 +6,12 @@ public class EchoClient {
     public static final int portNumber = 6013;
 
     public static void main(String[] args) throws IOException {
-      String server;
-      // Use "127.0.0.1", i.e., localhost, if no server is specified. Otherwise,
-      // use the server specified by the client.
-      if (args.length == 0) {
-        server = "127.0.0.1";
-      } else {
-        server = args[0];
-      }
+      EchoClient client = new EchoClient();
+      client.start();
+    }
+
+    private void start() throws IOException {
+      String server = "localhost";
   
       try {
         // Connect to the server
