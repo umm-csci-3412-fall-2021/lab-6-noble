02b81281c08e909cf10ace0d7a014389bce02cf3
Kyle Day <day00090@morris.umn.edu>
2021-11-02 10:42:58 -0500

    Update EchoClient.java with Thread Code in start Method
    This commit:
    
    + Creates input and output threads using the runnable classes
    
    + Implements a try and catch block to force the threads to wait while another is working
    
    + Renames input and output stream variable names to inStream and outStream respectively, in order to avoid confusion with other variable names

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -11,18 +11,43 @@ public class EchoClient {
     }
 
     private void start() throws IOException {
-      String server = "localhost";
+      final String server = "localhost";
   
       try {
         // Connect to the server
         Socket serverSocket = new Socket(server, portNumber);
   
         // Get the input stream so we can read from that socket
-        InputStream input = serverSocket.getInputStream();
+        InputStream inStream = serverSocket.getInputStream();
 
         // Get the output stream so we can write from that socket
-        OutputStream output = serverSocket.getOutputStream();
-  
+        OutputStream outStream = serverSocket.getOutputStream();
+
+        // Initialize the runnable writer drawing from the input
+        WriteFromInputThread inputWriter = new WriteFromInputThread();
+
+        // Creates a thread for the input, using the input stream from the socket
+        Thread inputThread = new Thread(inputWriter);
+
+        // Initialize the runnable writer drawing from the output
+        WriteFromOutputThread outputWriter = new WriteFromOutputThread();
+
+        // Creates a thread for the output, using the output stream from the socket
+        Thread outputThread = new Thread(outputWriter);
+
+        // Start both the input and output threads
+        inputThread.start();
+        outputThread.start();
+
+        // Have each of the threads wait their turn before acting to avoid race conditions and deadlock
+        // Throw exceptions if there are issues
+        try {
+          inputThread.join();
+          outputThread.join();
+        } catch (InterruptedException ie) {
+          System.out.println("We've caught an interrupted exception: " + ie.getMessage());
+        }
+
         // Initialize the variable to contain bytes sent to the server.
         int sentByte = System.in.read();
 
@@ -30,10 +55,10 @@ public class EchoClient {
         // to the client.
         while((sentByte) != -1) {
           // Write the byte taken in to the output stream for the socket.
-          output.write(sentByte);
+          outStream.write(sentByte);
             
           // Read a byte from the input stream and write it to the system's output.
-          int receivedByte = input.read();
+          int receivedByte = inStream.read();
           System.out.write(receivedByte);
 
           // Get the next byte being sent by the client.
@@ -42,7 +67,7 @@ public class EchoClient {
 
         //Flush the stream for the system and the server socket outputs.
         System.out.flush();
-        output.flush();
+        outStream.flush();
         // Shutdown the socket's output.
         serverSocket.shutdownOutput();
         // Close the socket.
@@ -60,13 +85,13 @@ public class EchoClient {
 
     public class WriteFromInputThread implements Runnable {
       public void run() {
-        
+        //
       }
     }
 
     public class WriteFromOutputThread implements Runnable {
       public void run() {
-
+        //
       }
     }
 }
