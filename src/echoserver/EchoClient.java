887e9092a00662429135d7f7dc0ee044c4578e67
Kyle Day <day00090@morris.umn.edu>
2021-11-03 20:53:13 -0500

    Remove Artifacts from Previous Lab Reference in EchoClient.java
    I removed the try and catch statements leftover from the previous lab, as they were inhibiting the client's functionality and lacked relevance.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -2,7 +2,6 @@ package echoserver;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.ConnectException;
 import java.net.Socket;
 
 public class EchoClient {
@@ -15,53 +14,42 @@ public class EchoClient {
 
     private void start() throws IOException {
       final String server = "localhost";
+      // Connect to the server
+      Socket serverSocket = new Socket(server, portNumber);
   
+      // Get the input stream so we can read from that socket
+      InputStream inStream = serverSocket.getInputStream();
+
+      // Get the output stream so we can write from that socket
+      OutputStream outStream = serverSocket.getOutputStream();
+
+      // Initialize the runnable writer drawing from the input
+      WriteFromInputThread inputWriter = new WriteFromInputThread(serverSocket, outStream);
+
+      // Creates a thread for the input, using the input stream from the socket
+      Thread inputThread = new Thread(inputWriter);
+
+      // Initialize the runnable writer drawing from the output
+      WriteFromOutputThread outputWriter = new WriteFromOutputThread(serverSocket, inStream);
+
+      // Creates a thread for the output, using the output stream from the socket
+      Thread outputThread = new Thread(outputWriter);
+
+      // Start both the input and output threads
+      inputThread.start();
+      outputThread.start();
+
+      // Have each of the threads wait their turn before acting to avoid race conditions and deadlock
+      // Throw exceptions if there are issues
       try {
-        // Connect to the server
-        Socket serverSocket = new Socket(server, portNumber);
-  
-        // Get the input stream so we can read from that socket
-        InputStream inStream = serverSocket.getInputStream();
-
-        // Get the output stream so we can write from that socket
-        OutputStream outStream = serverSocket.getOutputStream();
-
-        // Initialize the runnable writer drawing from the input
-        WriteFromInputThread inputWriter = new WriteFromInputThread(serverSocket, outStream);
-
-        // Creates a thread for the input, using the input stream from the socket
-        Thread inputThread = new Thread(inputWriter);
-
-        // Initialize the runnable writer drawing from the output
-        WriteFromOutputThread outputWriter = new WriteFromOutputThread(serverSocket, inStream);
-
-        // Creates a thread for the output, using the output stream from the socket
-        Thread outputThread = new Thread(outputWriter);
-
-        // Start both the input and output threads
-        inputThread.start();
-        outputThread.start();
-
-        // Have each of the threads wait their turn before acting to avoid race conditions and deadlock
-        // Throw exceptions if there are issues
-        try {
-          inputThread.join();
-          outputThread.join();
-        } catch (InterruptedException ie) {
-          System.out.println("We've caught an interrupted exception: " + ie.getMessage());
-        }
-        
-        // Once all the work is completed, close the server socket
-        serverSocket.close();
-  
-      // Provide some minimal error handling.
-      } catch (ConnectException ce) {
-        System.out.println("We were unable to connect to " + server + ".");
-        System.out.println("You should make sure the server is running.");
-      } catch (IOException ioe) {
-        System.out.println("We caught an unexpected exception:");
-        System.err.println(ioe);
+        inputThread.join();
+        outputThread.join();
+      } catch (InterruptedException ie) {
+        System.out.println("We've caught an interrupted exception: " + ie.getMessage());
       }
+        
+      // Once all the work is completed, close the server socket
+      serverSocket.close();
     }
 
     public class WriteFromInputThread implements Runnable {
