9380c51169d6bb5f1228763fbfbd69053f87cb95
Kyle Day <day00090@morris.umn.edu>
2021-11-03 21:18:24 -0500

    Fix Formatting in EchoClient.java
    In order to keep the indentation consistent, I made it so that all parts of the code are tabbed in once per level.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -5,120 +5,121 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class EchoClient {
-    public static final int portNumber = 6013;
+  public static final int portNumber = 6013;
 
-    public static void main(String[] args) throws IOException {
-      EchoClient client = new EchoClient();
-      client.start();
+  public static void main(String[] args) throws IOException {
+    EchoClient client = new EchoClient();
+    client.start();
+  }
+
+  private void start() throws IOException {
+    final String server = "localhost";
+    // Connect to the server
+    Socket serverSocket = new Socket(server, portNumber);
+
+    // Get the input stream so we can read from that socket
+    InputStream inStream = serverSocket.getInputStream();
+
+    // Get the output stream so we can write from that socket
+    OutputStream outStream = serverSocket.getOutputStream();
+
+    // Initialize the runnable writer drawing from the input
+    WriteFromInputThread inputWriter = new WriteFromInputThread(serverSocket, outStream);
+
+    // Creates a thread for the input, using the input stream from the socket
+    Thread inputThread = new Thread(inputWriter);
+
+    // Initialize the runnable writer drawing from the output
+    WriteFromOutputThread outputWriter = new WriteFromOutputThread(serverSocket, inStream);
+
+    // Creates a thread for the output, using the output stream from the socket
+    Thread outputThread = new Thread(outputWriter);
+
+    // Start both the input and output threads
+    inputThread.start();
+    outputThread.start();
+
+    // Have each of the threads wait their turn before acting to avoid race
+    // conditions and deadlock
+    // Throw exceptions if there are issues
+    try {
+      inputThread.join();
+      outputThread.join();
+    } catch (InterruptedException ie) {
+      System.out.println("We've caught an interrupted exception: " + ie.getMessage());
     }
 
-    private void start() throws IOException {
-      final String server = "localhost";
-      // Connect to the server
-      Socket serverSocket = new Socket(server, portNumber);
-  
-      // Get the input stream so we can read from that socket
-      InputStream inStream = serverSocket.getInputStream();
+    // Once all the work is completed, close the server socket
+    serverSocket.close();
+  }
 
-      // Get the output stream so we can write from that socket
-      OutputStream outStream = serverSocket.getOutputStream();
+  public class WriteFromInputThread implements Runnable {
+    Socket workSocket;
+    OutputStream workOutStream;
 
-      // Initialize the runnable writer drawing from the input
-      WriteFromInputThread inputWriter = new WriteFromInputThread(serverSocket, outStream);
+    public WriteFromInputThread(Socket sockWorkSocket, OutputStream sockWorkOutStream) {
+      this.workSocket = sockWorkSocket;
+      this.workOutStream = sockWorkOutStream;
+    }
 
-      // Creates a thread for the input, using the input stream from the socket
-      Thread inputThread = new Thread(inputWriter);
-
-      // Initialize the runnable writer drawing from the output
-      WriteFromOutputThread outputWriter = new WriteFromOutputThread(serverSocket, inStream);
-
-      // Creates a thread for the output, using the output stream from the socket
-      Thread outputThread = new Thread(outputWriter);
-
-      // Start both the input and output threads
-      inputThread.start();
-      outputThread.start();
-
-      // Have each of the threads wait their turn before acting to avoid race conditions and deadlock
-      // Throw exceptions if there are issues
+    public void run() {
       try {
-        inputThread.join();
-        outputThread.join();
-      } catch (InterruptedException ie) {
-        System.out.println("We've caught an interrupted exception: " + ie.getMessage());
-      }
-        
-      // Once all the work is completed, close the server socket
-      serverSocket.close();
-    }
+        // Initialize the variable to contain bytes sent to the server.
+        int sentByte = System.in.read();
 
-    public class WriteFromInputThread implements Runnable {
-      Socket workSocket;
-      OutputStream workOutStream;
-
-      public WriteFromInputThread(Socket sockWorkSocket, OutputStream sockWorkOutStream) {
-        this.workSocket = sockWorkSocket;
-        this.workOutStream = sockWorkOutStream;
-      }
-
-      public void run() {
-        try {
-          // Initialize the variable to contain bytes sent to the server.
-          int sentByte = System.in.read();
-
-          // Facilitate the writing and re-sending of the bytes sent to the server back
-          // to the client.
-          while((sentByte) != -1) {
+        // Facilitate the writing and re-sending of the bytes sent to the server back
+        // to the client.
+        while ((sentByte) != -1) {
           // Write the byte taken in to the output stream for the socket.
-            workOutStream.write(sentByte);
+          workOutStream.write(sentByte);
 
-            // Get the next byte being sent by the client.
-            sentByte = System.in.read();
+          // Get the next byte being sent by the client.
+          sentByte = System.in.read();
         }
 
         workOutStream.flush();
 
         workSocket.shutdownOutput();
 
-        } catch (IOException ioe) {
-          System.out.println("We caught an unexpected exception while reading the input:");
-          System.err.println(ioe);
+      } catch (IOException ioe) {
+        System.out.println("We caught an unexpected exception while reading the input:");
+        System.err.println(ioe);
+      }
+    }
+  }
+
+  public class WriteFromOutputThread implements Runnable {
+    Socket workSocket;
+    InputStream workInStream;
+
+    public WriteFromOutputThread(Socket sockWorkSocket, InputStream sockWorkInStream) {
+      this.workSocket = sockWorkSocket;
+      this.workInStream = sockWorkInStream;
+    }
+
+    public void run() {
+      try {
+        // Initialize the variable to contain bytes sent to the server.
+        int receivedByte = workInStream.read();
+
+        // Facilitate the writing and re-sending of the bytes sent to the server back
+        // to the client.
+        while ((receivedByte) != -1) {
+          // Write the byte taken in to the output stream for the socket.
+
+          // Read a byte from the input stream and write it to the system's output.
+          System.out.write(receivedByte);
+          receivedByte = workInStream.read();
         }
+
+        System.out.flush();
+
+        workSocket.shutdownInput();
+
+      } catch (IOException ioe) {
+        System.out.println("We caught an unexpected exception while reading the input:");
+        System.err.println(ioe);
       }
     }
-
-    public class WriteFromOutputThread implements Runnable {
-      Socket workSocket;
-      InputStream workInStream;
-
-      public WriteFromOutputThread(Socket sockWorkSocket, InputStream sockWorkInStream) {
-        this.workSocket = sockWorkSocket;
-        this.workInStream = sockWorkInStream;
-      }
-
-      public void run() {
-          try {
-            // Initialize the variable to contain bytes sent to the server.
-              int receivedByte = workInStream.read();
-  
-            // Facilitate the writing and re-sending of the bytes sent to the server back
-            // to the client.
-            while((receivedByte) != -1) {
-              // Write the byte taken in to the output stream for the socket.
-              
-              // Read a byte from the input stream and write it to the system's output.
-              System.out.write(receivedByte);
-              receivedByte = workInStream.read();
-          }
-
-          System.out.flush();
-
-          workSocket.shutdownInput();
-
-          } catch (IOException ioe) {
-            System.out.println("We caught an unexpected exception while reading the input:");
-            System.err.println(ioe);
-          }
-      }
-    }
+  }
 }
