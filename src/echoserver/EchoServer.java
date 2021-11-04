841070191c51053df36a34e2f07f650af1a49df3
Kyle Day <day00090@morris.umn.edu>
2021-11-03 21:37:03 -0500

    Split EchoServer.java into main and start Methods
    Like EchoClient.java, in order to add threads to the server, we will follow the main-start method "convention" for working with threads. Thus, a new server object is create in main, while the start method is used to do the work of the server.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -8,45 +8,45 @@ import java.net.Socket;
 public class EchoServer {
   public static final int portNumber = 6013;
 
-  public static void main(String[] args) {
-    try {
-      // Start listening on the specified port
-      ServerSocket serverSocket = new ServerSocket(portNumber);
+  public static void main(String[] args) throws IOException {
+    EchoServer server = new EchoServer();
+    server.start();
+  }
 
-      // Run forever, which is common for server style services
-      while (true) {
-        // Wait until someone connects, thereby requesting a date
-        Socket clientSocket = serverSocket.accept();
-        System.out.println("Got a request!");
-        
-        // Get the input stream from the client's socket so that we can read from it
-        InputStream input = clientSocket.getInputStream();
+  private void start() throws IOException {
+    // Start listening on the specified port
+    ServerSocket serverSocket = new ServerSocket(portNumber);
 
-        // Get the output stream of the client's socket so that we can write to it
-        OutputStream output = clientSocket.getOutputStream();
+    // Run forever, which is common for server style services
+    while (true) {
+      // Wait until someone connects, thereby requesting a date
+      Socket clientSocket = serverSocket.accept();
+      System.out.println("Got a request!");
 
-        // A variable for storing the bytes sent
-        int sentByte;
+      // Get the input stream from the client's socket so that we can read from it
+      InputStream input = clientSocket.getInputStream();
 
-        // Facilitate the writing of the bytes sent and received to the client's output
-        // stream
-        while((sentByte = input.read()) != -1) {
-          // Write a byte to the output
-          output.write(sentByte);
-          // Flush the output after each write.
-          output.flush();
-        }
-        
-        System.out.println("The request has been processed.");
-        // Shutdown the output of the client socket so that no more bytes can be written to it
-        clientSocket.shutdownOutput();
-        // Close the client socket completely
-        clientSocket.close();
+      // Get the output stream of the client's socket so that we can write to it
+      OutputStream output = clientSocket.getOutputStream();
+
+      // A variable for storing the bytes sent
+      int sentByte;
+
+      // Facilitate the writing of the bytes sent and received to the client's output
+      // stream
+      while ((sentByte = input.read()) != -1) {
+        // Write a byte to the output
+        output.write(sentByte);
+        // Flush the output after each write.
+        output.flush();
       }
-    // *Very* minimal error handling.
-    } catch (IOException ioe) {
-      System.out.println("We caught an unexpected exception:");
-      System.err.println(ioe);
+
+      System.out.println("The request has been processed.");
+      // Shutdown the output of the client socket so that no more bytes can be written
+      // to it
+      clientSocket.shutdownOutput();
+      // Close the client socket completely
+      clientSocket.close();
     }
   }
 }
