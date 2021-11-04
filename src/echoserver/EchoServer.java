74b5eab7d852d47e247014b9699bfa09bf5e0f9a
Kyle Day <day00090@morris.umn.edu>
2021-11-03 21:27:19 -0500

    Add Relevant Imports to EchoServer.java
    In preparation for building the server code, I have added the necessary imports needed for its construction. We'll need IOException for display an input/output error to the user, the InputStream and OutputStream imports for working with their related streams, and ServerSocket and Socket for working with sockets.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,6 +1,9 @@
 package echoserver;
-import java.net.*;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class EchoServer {
   public static final int portNumber = 6013;
